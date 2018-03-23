426c8ef4dc8f6f9bf5071e2a166596dff3b9b42b
Alexandr Nikitenko <sashanikitenko74@gmail.com>
2018-03-23 11:45:42 +0200

    Create firstDigit.py

---
+++ Diving Deeper/firstDigit.py
@@ -0,0 +1,28 @@
+"""
+Find the leftmost digit that occurs in a given string.
+
+Example
+
+    For inputString = "var_1__Int", the output should be
+    firstDigit(inputString) = '1';
+    For inputString = "q2q-q", the output should be
+    firstDigit(inputString) = '2';
+    For inputString = "0ss", the output should be
+    firstDigit(inputString) = '0'.
+
+Input/Output
+
+    [execution time limit] 4 seconds (py3)
+
+    [input] string inputString
+
+    A string containing at least one digit.
+
+    Guaranteed constraints:
+    3 ≤ inputString.length ≤ 10.
+
+    [output] char
+"""
+
+def firstDigit(inputString):
+    return [i for i in inputString if i.isdigit()][0]
